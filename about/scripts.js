636d2f2669381df9f98aed94d063071e9a589433
morningbell <brianna124@gmail.com>
2018-12-03 22:03:01 -0500

    Finished responsive styles for About page and added more content.

--- about/scripts.js
+++ about/scripts.js
@@ -6,3 +6,12 @@ const divToToggle = document.querySelector("#more-brian");
 button.addEventListener("click", ()=>{
   divToToggle.classList.toggle("hide");
 });
+
+// Show More-Paco div when "More About Brian" button is clicked
+
+const buttonPaco = document.querySelector(".aboutBtnPaco");
+const divToTogglePaco = document.querySelector("#more-paco");
+
+buttonPaco.addEventListener("click", ()=>{
+  divToTogglePaco.classList.toggle("hide");
+});
