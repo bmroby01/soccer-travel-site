5825104b51d5794424768b7dde3e7ef88fc10b0f
morningbell <brianna124@gmail.com>
2018-12-04 16:41:44 -0500

    Testing bug fix for IE.

--- about/scripts.js
+++ about/scripts.js
@@ -1,3 +1,21 @@
+// // Show More-Brian div when "More About Brian" button is clicked
+//
+// const button = document.querySelector(".aboutBtn");
+// const divToToggle = document.querySelector("#more-brian");
+//
+// button.addEventListener("click", ()=>{
+//   divToToggle.classList.toggle("hide");
+// });
+//
+// // Show More-Paco div when "More About Brian" button is clicked
+//
+// const buttonPaco = document.querySelector(".aboutBtnPaco");
+// const divToTogglePaco = document.querySelector("#more-paco");
+//
+// buttonPaco.addEventListener("click", ()=>{
+//   divToTogglePaco.classList.toggle("hide");
+// });
+
 // Show More-Brian div when "More About Brian" button is clicked
 
 const button = document.querySelector(".aboutBtn");
