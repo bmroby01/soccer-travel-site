ccd2f806b0f3df942b826468e750ab364ae3c9d7
morningbell <brianna124@gmail.com>
2018-12-04 16:44:44 -0500

    New test for IE bug.

--- about/scripts.js
+++ about/scripts.js
@@ -18,18 +18,22 @@
 
 // Show More-Brian div when "More About Brian" button is clicked
 
-const button = document.querySelector(".aboutBtn");
-const divToToggle = document.querySelector("#more-brian");
+"use strict";
 
-button.addEventListener("click", ()=>{
+// Show More-Brian div when "More About Brian" button is clicked
+
+var button = document.querySelector(".aboutBtn");
+var divToToggle = document.querySelector("#more-brian");
+
+button.addEventListener("click", function () {
   divToToggle.classList.toggle("hide");
 });
 
 // Show More-Paco div when "More About Brian" button is clicked
 
-const buttonPaco = document.querySelector(".aboutBtnPaco");
-const divToTogglePaco = document.querySelector("#more-paco");
+var buttonPaco = document.querySelector(".aboutBtnPaco");
+var divToTogglePaco = document.querySelector("#more-paco");
 
-buttonPaco.addEventListener("click", ()=>{
+buttonPaco.addEventListener("click", function () {
   divToTogglePaco.classList.toggle("hide");
 });
